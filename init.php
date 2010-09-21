47610e110e516bac67c67cd5875a552a92833b5b
czukowski <carbofos@seznam.cz>
2010-09-21 10:41:43 +0200

    Updated ___()

--- init.php
+++ init.php
@@ -14,6 +14,11 @@
  */
 function ___($string, $count = 0, array $values = NULL)
 {
+	if (is_array($count) AND $values === NULL)
+	{
+		$values = $count;
+		$count = 0;
+	}
 	$string = I18n_Plural::get($string, $count);
 	return empty($values) ? $string : strtr($string, $values);
 }
