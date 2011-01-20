0c28c3c6202ff2996703678afa438ea855b87526
czukowski <carbofos@seznam.cz>
2011-01-20 08:38:16 +0100

    Renamed parameter  to

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
- * Kohana translation/internationalization function with custom forms support.
+ * Kohana translation/internationalization function with context support.
  * The PHP function [strtr](http://php.net/strtr) is used for replacing parameters.
  *
  *    ___(':count user is online', 1000, array(':count' => 1000));
@@ -9,29 +9,29 @@
  * @uses I18n_Plural::get()
  * @uses I18n_Form::get()
  * @param string $string to translate
- * @param mixed $count string form or numeric count
+ * @param mixed $context string form or numeric count
  * @param array $values param values to insert
  * @param string $lang target language
  * @return string
  */
-function ___($string, $count = 0, $values = NULL, $lang = NULL)
+function ___($string, $context = 0, $values = NULL, $lang = NULL)
 {
-	if (is_array($count) AND ! is_array($values))
+	if (is_array($context) AND ! is_array($values))
 	{
 		// Assume no form is specified and the 2nd argument are parameters
 		$lang = $values;
-		$values = $count;
-		$count = 0;
+		$values = $context;
+		$context = 0;
 	}
-	if (is_numeric($count))
+	if (is_numeric($context))
 	{
 		// Get plural form
-		$string = I18n_Plural::get($string, $count, $lang);
+		$string = I18n_Plural::get($string, $context, $lang);
 	}
 	else
 	{
 		// Get custom form
-		$string = I18n_Form::get($string, $count, $lang);
+		$string = I18n_Form::get($string, $context, $lang);
 	}
 	return empty($values) ? $string : strtr($string, $values);
 }
