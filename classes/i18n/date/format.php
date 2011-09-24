4a87bae1cdda72a8810850574903ff8ffd787684
Yan Ivanov <romulus@yandex.ru>
2011-09-25 03:42:06 +1100

    moved default date formats to config file

--- classes/i18n/date/format.php
+++ classes/i18n/date/format.php
@@ -16,16 +16,7 @@ class I18n_Date_Format extends Kohana_Date
 	 * Named formats
 	 * @var array
 	 */
-	protected $_formats = array(
-		'db' => '%Y-%m-%d %H:%M:%S',
-		'compact' => '%Y%m%dT%H%M%S',
-		'header' => '%g',
-		'iso8601' => '%Y-%m-%dT%H:%M:%S%T',
-		'rfc822' => '%a, %d %b %Y %H:%M:%S %z',
-		'rfc2822' => '%r',
-		'short' => '%d %b %H:%M',
-		'long' => '%B %d, %Y %H:%M',
-	);
+	protected $_formats = array();
 	/**
 	 * @var int
 	 */
