4ced676ab56a92b371d8a9474d481aacb4beed4f
Korney Czukowski <carbofos@seznam.cz>
2012-01-04 17:39:29 +0100

    Fixed access level

--- classes/i18n/unittest/core.php
+++ classes/i18n/unittest/core.php
@@ -12,12 +12,12 @@ abstract class I18n_Unittest_Core extends Kohana_Unittest_Testcase
 {
 	protected $langBackup;
 
-	protected function setUp() {
+	public function setUp() {
 		$this->langBackup = I18n::lang();
 		parent::setUp();
 	}
 
-	protected function tearDown() {
+	public function tearDown() {
 		I18n::lang($this->langBackup);
 		parent::tearDown();
 	}
