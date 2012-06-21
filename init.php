7acaaa7b6fb8cc31024dc355ba788750104c3eb5
Korney Czukowski <carbofos@seznam.cz>
2012-06-21 07:36:40 +0200

    The ___() function uses initialized I18n_Core object

--- init.php
+++ init.php
@@ -1,13 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
- * Kohana translation/internationalization function with context support.
+ * Kohana translation/internationalization convenience function with context support.
  * The PHP function [strtr](http://php.net/strtr) is used for replacing parameters.
- *
+ * 
  *    ___(':count user is online', 1000, array(':count' => 1000));
  *    // 1000 users are online
- *
- * @uses    I18n_Plural::get()
- * @uses    I18n_Form::get()
+ * 
+ * @uses    I18n_Core::translate()
  * @param   string  $string to translate
  * @param   mixed   $context string form or numeric count
  * @param   array   $values param values to insert
@@ -16,25 +15,8 @@
  */
 function ___($string, $context = 0, $values = NULL, $lang = NULL)
 {
-	if (is_array($context) AND ! is_array($values))
-	{
-		// Assume no form is specified and the 2nd argument are parameters
-		$lang = $values;
-		$values = $context;
-		$context = 0;
-	}
-	if (is_numeric($context))
-	{
-		// Get plural form
-		$string = I18n::plural($string, $context, $lang);
-	}
-	else
-	{
-		// Get custom form
-		$string = I18n::form($string, $context, $lang);
-	}
-	return empty($values) ? $string : strtr($string, $values);
+	return I18n_Core::instance()->translate($string, $context, $values, $lang);
 }
 
-// Force load I18n class, if not done already
-I18n::lang();
+// Initialize I18n_Core object. It'll be then accessible via `I18n_Core::instance()`
+new I18n_Core(new I18n_Reader_Kohana);
