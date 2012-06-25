a1f1d239f2a8f5c19534859c4128b90238c5c338
Korney Czukowski <carbofos@seznam.cz>
2012-06-25 18:05:05 +0200

    Updated comment

--- init.php
+++ init.php
@@ -3,19 +3,18 @@
 if ( ! function_exists('___'))
 {
 	/**
-	* Kohana translation/internationalization convenience function with context support.
-	* The PHP function [strtr](http://php.net/strtr) is used for replacing parameters.
-	* 
-	*    ___(':count user is online', 1000, array(':count' => 1000));
-	*    // 1000 users are online
-	* 
-	* @uses    I18n_Core::translate()
-	* @param   string  $string to translate
-	* @param   mixed   $context string form or numeric count
-	* @param   array   $values param values to insert
-	* @param   string  $lang target language
-	* @return  string
-	*/
+	 * This is a gateway to the `I18n_Core` functions, as there's no static access to it.
+	 * 
+	 *    ___(':count user is online', 1000, array(':count' => 1000));
+	 *    // 1000 users are online
+	 * 
+	 * @uses    I18n_Core::translate()
+	 * @param   string  $string to translate
+	 * @param   mixed   $context string form or numeric count
+	 * @param   array   $values param values to insert
+	 * @param   string  $lang target language
+	 * @return  string
+	 */
 	function ___($string, $context = 0, $values = NULL, $lang = NULL)
 	{
 		static $i18n;
