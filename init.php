4bd1f4edf93fd42021fa62d2f3348d40e26d4108
Korney Czukowski <carbofos@seznam.cz>
2012-06-25 18:03:31 +0200

    Checking for ___() function existence

--- init.php
+++ init.php
@@ -1,29 +1,33 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-/**
- * Kohana translation/internationalization convenience function with context support.
- * The PHP function [strtr](http://php.net/strtr) is used for replacing parameters.
- * 
- *    ___(':count user is online', 1000, array(':count' => 1000));
- *    // 1000 users are online
- * 
- * @uses    I18n_Core::translate()
- * @param   string  $string to translate
- * @param   mixed   $context string form or numeric count
- * @param   array   $values param values to insert
- * @param   string  $lang target language
- * @return  string
- */
-function ___($string, $context = 0, $values = NULL, $lang = NULL)
+
+if ( ! function_exists('___'))
 {
-	static $i18n;
-	if ($i18n === NULL)
+	/**
+	* Kohana translation/internationalization convenience function with context support.
+	* The PHP function [strtr](http://php.net/strtr) is used for replacing parameters.
+	* 
+	*    ___(':count user is online', 1000, array(':count' => 1000));
+	*    // 1000 users are online
+	* 
+	* @uses    I18n_Core::translate()
+	* @param   string  $string to translate
+	* @param   mixed   $context string form or numeric count
+	* @param   array   $values param values to insert
+	* @param   string  $lang target language
+	* @return  string
+	*/
+	function ___($string, $context = 0, $values = NULL, $lang = NULL)
 	{
-		// Initialize I18n_Core object
-		$i18n = new I18n_Core(new I18n_Reader_Kohana);
+		static $i18n;
+		if ($i18n === NULL)
+		{
+			// Initialize I18n_Core object
+			$i18n = new I18n_Core(new I18n_Reader_Kohana);
+		}
+		if ($lang === NULL)
+		{
+			$lang = I18n::lang();
+		}
+		return $i18n->translate($string, $context, $values, $lang);
 	}
-	if ($lang === NULL)
-	{
-		$lang = I18n::lang();
-	}
-	return $i18n->translate($string, $context, $values, $lang);
 }
