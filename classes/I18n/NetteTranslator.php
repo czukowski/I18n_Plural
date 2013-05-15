112e4e1619de193811b68a9bcdd4c22c159616fa
Korney Czukowski <carbofos@seznam.cz>
2013-05-15 12:37:07 +0200

    Implemented Nette adapter

--- classes/I18n/NetteTranslator.php
+++ classes/I18n/NetteTranslator.php
@@ -9,5 +9,50 @@
  */
 namespace I18n;
 
-class NetteTranslator extends Core implements \Nette\Localization\ITranslator
-{}
+class NetteTranslator implements \Nette\Localization\ITranslator
+{
+	/**
+	 * @var  Core
+	 */
+	private $i18n;
+
+	/**
+	 * Instanciates a new I18n\Core object.
+	 */
+	public function __construct()
+	{
+		$this->i18n = new Core;
+	}
+
+	/**
+	 * Attach an i18n reader to a core object.
+	 * 
+	 * @param  \I18n\Reader\ReaderInterface  $reader
+	 */
+	public function attach(Reader\ReaderInterface $reader)
+	{
+		$this->i18n->attach($reader);
+	}
+
+	/**
+	 * Nette localization interface adapter. Does not support parameters replacement.
+	 * 
+	 * @param   string  $string
+	 * @param   mixed   $count
+	 * @return  string
+	 */
+	public function translate($string, $count = NULL)
+	{
+		return $this->i18n->translate($string, $count, array());
+	}
+
+	/**
+	 * Retrieves the core object instance for direct usage.
+	 * 
+	 * @return  Core
+	 */
+	public function getService()
+	{
+		return $this->i18n;
+	}
+}
