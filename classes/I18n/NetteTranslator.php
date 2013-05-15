9cd481ed81fd64ebd5193cf281b79b12c9ccd542
Korney Czukowski <carbofos@seznam.cz>
2013-05-15 11:36:51 +0200

    Added Nette Translator class

---
+++ classes/I18n/NetteTranslator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Nette Translator adapter class
+ * 
+ * @package    I18n
+ * @author     Korney Czukowski
+ * @copyright  (c) 2013 Korney Czukowski
+ * @license    MIT License
+ */
+namespace I18n;
+
+class NetteTranslator extends Core implements \Nette\Localization\ITranslator
+{}
